c375e010cb076fec525731db0418b7254ab52d21
bensatlantik <bensantora@proton.me>
2024-11-02 18:18:54 -0400

    Initial commit of linux_terminal_notify

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub enum NotificationType {
+    Info,
+    Warning,
+    Error,
+}
+
+pub fn notify(msg: &str, ntype: NotificationType) {
+    match ntype {
+        NotificationType::Info => println!("\x1b[32m[INFO]\x1b[0m {}", msg),
+        NotificationType::Warning => println!("\x1b[33m[WARNING]\x1b[0m {}", msg),
+        NotificationType::Error => println!("\x1b[31m[ERROR]\x1b[0m {}", msg),
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +17,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_info_notification() {
+        notify("This is an info message.", NotificationType::Info);
+    }
+
+    #[test]
+    fn test_warning_notification() {
+        notify("This is a warning message.", NotificationType::Warning);
+    }
+
+    #[test]
+    fn test_error_notification() {
+        notify("This is an error message.", NotificationType::Error);
     }
 }
